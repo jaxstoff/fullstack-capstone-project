41dcf1fe45af98477f3a657f571736d8d818be39
Andy Stoffel <acstoffel@gmail.com>
2025-03-26 15:11:27 -0400

    added missing template code and cleaned up comments

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,33 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
+
+// get all gifts
 router.get('/', async (req, res) => {
+    logger.info('/ called');
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
+        // Task 1: Connect to MongoDB 
         const db = await connectToDatabase();
 
-        // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
-
-        // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
         const gifts = await collection.find({}).toArray();
-
-        // Task 4: return the gifts using the res.json method
         res.json(gifts);
     } catch (e) {
-        console.error('Error fetching gifts:', e);
-        res.status(500).send('Error fetching gifts');
+        logger.console.error('Error - something went wrong fetching gifts:', e);
+        next(e);
     }
 });
 
+// get a gift by id
 router.get('/:id', async (req, res) => {
     try {
-        // Task 1: Connect to MongoDB and store connection to db constant
         const db = await connectToDatabase();
-
-        // Task 2: use the collection() method to retrieve the gift collection
         const collection = db.collection("gifts");
-
         const id = req.params.id;
-
-        // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
         const gift = await collection.findOne({ id: id });
 
         if (!gift) {
@@ -36,8 +33,7 @@ router.get('/:id', async (req, res) => {
 
         res.json(gift);
     } catch (e) {
-        console.error('Error fetching gift:', e);
-        res.status(500).send('Error fetching gift');
+        next(e);
     }
 });
 
