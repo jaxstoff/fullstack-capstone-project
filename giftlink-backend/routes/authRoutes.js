274f97028fb680556d30c7e81ab018d1a67730df
Andy Stoffel <acstoffel@gmail.com>
2025-04-02 16:03:01 -0400

    Module 4 - Lab 3 - Login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -7,13 +7,12 @@ const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
-
 const logger = pino();  // Create a Pino logger instance
 
 dotenv.config();
-
 const JWT_SECRET = process.env.JWT_SECRET;
 
+// Register
 router.post('/register', async (req, res) => {
     try {
         // Connect to `giftsdb` in MongoDB 
@@ -24,6 +23,10 @@ router.post('/register', async (req, res) => {
 
         // Check for existing email
         const existingEmail = await collection.findOne({ email: req.body.email });
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
 
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
@@ -43,12 +46,48 @@ router.post('/register', async (req, res) => {
                 id: newUser.insertedId,
             },
         };
-
+        // create JWT
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
-        res.json({authtoken,email});
+        res.json({ authtoken, email });
     } catch (e) {
-         return res.status(500).send('Internal server error');
+        logger.error(e);
+        return res.status(500).send('Internal server error');
+    }
+});
+// Login
+router.post('/login', async (req, res) => {
+    console.log("\n\n Inside login")
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        const theUser = await collection.findOne({ email: req.body.email });
+        if (theUser) {
+            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            if (!result) {
+                logger.error('Passwords do not match');
+                return res.status(404).json({ error: 'Wrong pasword' });
+            }
+            let payload = {
+                user: {
+                    id: theUser._id.toString(),
+                },
+            };
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged in successfully');
+            return res.status(200).json({ authtoken, userName, userEmail });
+            // Final/task version mismatch - authtoken wasn't being created
+            //jwt.sign(user._id, JWT_SECRET);
+            //res.json({ authtoken, userName, userEmail });
+        } else {
+            logger.error('User not found');
+            return res.status(500).json({ error: 'Internal server error', details: e.message });
+        }
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
     }
 });
 
