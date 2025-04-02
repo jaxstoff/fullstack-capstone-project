532188f72bacdfa52f9c8a799b396b01abe6fd94
Andy Stoffel <acstoffel@gmail.com>
2025-04-02 00:41:36 -0400

    Module 3 Lab 5 SearchPage

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -7,6 +7,7 @@ import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
           <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
         </Routes>
         </>
   );
