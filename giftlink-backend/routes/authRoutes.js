0499c56e2e8a882717b7d023d43b39a85f18b44b
Andy Stoffel <acstoffel@gmail.com>
2025-04-02 23:59:12 -0400

    Module 4 - Lab 5 update backend

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
 const logger = pino();  // Create a Pino logger instance
+const { body, validationResult } = require('express-validator');
 
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -91,4 +91,65 @@ router.post('/login', async (req, res) => {
     }
 });
 
+// Update
+router.put('/update', async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+
+        const existingUser = await collection.findOne({ email });
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
+        // Task between 5 & 6?
+        // update first, lastname - need to do some validation here also?
+        // Update password separately?
+        const firstName = req.body.firstname;
+        if (firstName) {
+            existingUser.firstName = firstName;
+            logger.info(`User firstname updating to ${firstName}`);
+        }
+        const lastName = req.body.lastname;
+        if (lastName) {
+            existingUser.lastName = lastName;
+            logger.info(`User lastname updating to ${lastName}`);
+        }
+
+        existingUser.updatedAt = new Date();
+
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User updated successfully');
+
+        res.json({ authtoken });
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).send('Internal server error');
+
+    }
+});
 module.exports = router;
